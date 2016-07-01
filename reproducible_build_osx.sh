3e73b349b1083e472de35ee9b7204ce506024b53
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2016-07-01 11:56:31 -0500

    Capture dependency build output
    Travis is failing because the log is too big. There is a 4mb limit.
    Piping the build outputs to log files to reduce the total log size.

--- reproducible_build_osx.sh
+++ reproducible_build_osx.sh
@@ -11,18 +11,23 @@ function HELP {
     echo "Optional arguments:"
     echo
     echo "-c: clone a fresh copy of the repo"
+    echo "-r: remove intermediate files."
     echo "-h: show help"
     echo "-t: turn trace on"
     exit 1
 }
 
 CLONE=false
+CLEAN=false
 
 
-while getopts :hctb:w:d: FLAG; do
+while getopts :hrctb:w:d: FLAG; do
     case $FLAG in
 	c)
 	    CLONE=true
+	    ;;
+	r)
+	    CLEAN=true
 	    ;;
 	t)
 	    set -o xtrace
@@ -62,9 +67,14 @@ if ! brew ls | grep gmp --quiet; then
     brew install gmp
 fi
 
+if [ "$CLEAN" = true ]; then
+    rm -rf dependencies
+fi
 mkdir dependencies
+mkdir dependencies/log
 cd dependencies
 export LBRYCRD_DEPENDENCIES="`pwd`"
+LOG_DIR="${LBRYCRD_DEPENDENCIES}/log"
 
 #download, patch, and build bdb
 wget http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
@@ -73,9 +83,11 @@ export BDB_PREFIX="`pwd`/bdb"
 curl -OL https://raw.github.com/narkoleptik/os-x-berkeleydb-patch/master/atomic.patch
 patch db-4.8.30.NC/dbinc/atomic.h < atomic.patch
 cd db-4.8.30.NC/build_unix
-../dist/configure --prefix=$BDB_PREFIX --enable-cxx --disable-shared --with-pic
-make
-make install
+BDB_LOG="${LOG_DIR}/bdb_build.log"
+echo "Building bdb.  tail -f $BDB_LOG to see the details and monitor progress"
+../dist/configure --prefix=$BDB_PREFIX --enable-cxx --disable-shared --with-pic > "${BDB_LOG}"
+make >> "${BDB_LOG}" 2>&1
+make install >> "${BDB_LOG}" 2>&1
 
 #download and build openssl
 cd $LBRYCRD_DEPENDENCIES
@@ -85,9 +97,11 @@ export OPENSSL_PREFIX="`pwd`/openssl_build"
 mkdir $OPENSSL_PREFIX
 mkdir $OPENSSL_PREFIX/ssl
 cd openssl-1.0.1p
-./Configure --prefix=$OPENSSL_PREFIX --openssldir=$OPENSSL_PREFIX/ssl -fPIC darwin64-x86_64-cc no-shared no-dso no-engines
-make
-make install
+OPENSSL_LOG="${LOG_DIR}/openssl_build.log"
+echo "Building bdb.  tail -f $OPENSSL_LOG to see the details and monitor progress"
+./Configure --prefix=$OPENSSL_PREFIX --openssldir=$OPENSSL_PREFIX/ssl -fPIC darwin64-x86_64-cc no-shared no-dso no-engines > "${OPENSSL_LOG}"
+make >> "${OPENSSL_LOG}" 2>&1
+make install >> "${OPENSSL_LOG}" 2>&1
 
 #download and build boost
 cd $LBRYCRD_DEPENDENCIES
@@ -95,8 +109,10 @@ wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.b
 tar xf boost_1_59_0.tar.bz2
 export BOOST_ROOT="`pwd`/boost_1_59_0"
 cd boost_1_59_0
-./bootstrap.sh
-./b2 link=static cxxflags=-fPIC stage
+BOOST_LOG="${LOG_DIR}/boost_build.log"
+echo "Building Boost.  tail -f ${BOOST_LOG} to see the details and monitor progress"
+./bootstrap.sh > "${BOOST_LOG}"
+./b2 link=static cxxflags=-fPIC stage >> "${BOOST_LOG}"
 
 #download and build libevent
 cd $LBRYCRD_DEPENDENCIES
@@ -104,10 +120,13 @@ mkdir libevent_build
 git clone https://github.com/libevent/libevent.git
 export LIBEVENT_PREFIX="`pwd`/libevent_build"
 cd libevent
-./autogen.sh
-./configure --prefix=$LIBEVENT_PREFIX --enable-static --disable-shared --with-pic LDFLAGS="-L${OPENSSL_PREFIX}/lib/" CPPFLAGS="-I${OPENSSL_PREFIX}/include"
-make
-make install
+LIBEVENT_LOG="${LOG_DIR}/libevent_build.log"
+echo "Building libevent.  tail -f ${LIBEVENT_LOG} to see the details and monitor progress"
+./autogen.sh > "${LIBEVENT_LOG}"
+./configure --prefix=$LIBEVENT_PREFIX --enable-static --disable-shared --with-pic LDFLAGS="-L${OPENSSL_PREFIX}/lib/" CPPFLAGS="-I${OPENSSL_PREFIX}/include" >> "${LIBEVENT_LOG}"
+make >> "${LIBEVENT_LOG}"
+make install >> "${LIBEVENT_LOG}"
+
 set +u
 export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${OPENSSL_PREFIX}/lib/pkgconfig/:${LIBEVENT_PREFIX}/lib/pkgconfig/"
 set -u
