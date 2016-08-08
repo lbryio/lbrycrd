50c2ed0feebcc8863dbe760c77de828a82af3382
kkurokawa <umpeikurokawa@gmail.com>
2016-08-08 18:42:31 -0400

    lowering dust threshold to 1 , in order to not break lbrynet

--- src/test/transaction_tests.cpp
+++ src/test/transaction_tests.cpp
@@ -337,8 +337,11 @@ BOOST_AUTO_TEST_CASE(test_IsStandard)
     BOOST_CHECK(IsStandardTx(t, reason));
 
     // Check dust with default relay fee:
-    CAmount nDustThreshold = 182 * minRelayTxFee.GetFeePerK()/1000 * 3;
-    BOOST_CHECK_EQUAL(nDustThreshold, 546);
+    //CAmount nDustThreshold = 182 * minRelayTxFee.GetFeePerK()/1000 * 3;
+    //BOOST_CHECK_EQUAL(nDustThreshold, 546);
+    // lbry dust is set to 1, regardless of minRelayTxfee
+    CAmount nDustThreshold = 1;
+
     // dust:
     t.vout[0].nValue = nDustThreshold - 1;
     BOOST_CHECK(!IsStandardTx(t, reason));
