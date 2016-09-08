dc2d543be1bdb00fdfdde8873f3a395e729082de
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2016-09-08 09:47:01 +0200

    getinfo: only report rpc errors
    main.cpp GetWarnings() has a special parameter for those
    
    affect getinfo/getmininginfo errors fields
    
    getnetworkinfo "warnings" field was left over

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -329,7 +329,7 @@ UniValue getmininginfo(const UniValue& params, bool fHelp)
     obj.push_back(Pair("currentblocksize", (uint64_t)nLastBlockSize));
     obj.push_back(Pair("currentblocktx",   (uint64_t)nLastBlockTx));
     obj.push_back(Pair("difficulty",       (double)GetDifficulty()));
-    obj.push_back(Pair("errors",           GetWarnings("statusbar")));
+    obj.push_back(Pair("errors",           GetWarnings("rpc")));
     obj.push_back(Pair("genproclimit",     (int)GetArg("-genproclimit", -1)));
     obj.push_back(Pair("networkhashps",    getnetworkhashps(params, false)));
     obj.push_back(Pair("pooledtx",         (uint64_t)mempool.size()));
