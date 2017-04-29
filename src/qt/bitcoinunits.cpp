d438e9f9683b1f8bdccd3e57b3aabe0972518db1
mkg20001 <mkg20001@gmail.com>
2017-04-29 10:27:52 +0200

    Change CURRENCY_UNIT and BitcoinUnits::name
    This will show LBRY instead of BTC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("LBRY");
+    case mBTC: return QString("mLBRY");
+    case uBTC: return QString::fromUtf8("μLBRY");
     default: return QString("???");
     }
 }
