12013d8c7584f3846fedd9d9a5135687f5fabde3
kaykurokawa <umpeikurokawa@gmail.com>
2018-05-30 14:58:13 -0700

    normalize difficulty so that 1 == minmum difficulty

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -48,12 +48,12 @@ double GetDifficulty(const CBlockIndex* blockindex)
     double dDiff =
         (double)0x0000ffff / (double)(blockindex->nBits & 0x00ffffff);
 
-    while (nShift < 29)
+    while (nShift < 31)
     {
         dDiff *= 256.0;
         nShift++;
     }
-    while (nShift > 29)
+    while (nShift > 31)
     {
         dDiff /= 256.0;
         nShift--;
