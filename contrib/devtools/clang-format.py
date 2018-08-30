69ee02146988ff38d0e32d738ed5024a1c31d7ab
Brannon King <countprimes@gmail.com>
2018-08-30 14:18:48 -0600

    added scripts to generate parsable API docs (#187)
    also included the generated docs
    also changed Travis build script to not run the formatting check on OSX

--- contrib/devtools/clang-format.py
+++ contrib/devtools/clang-format.py
@@ -12,20 +12,7 @@ import os
 import sys
 import subprocess
 
-tested_versions = ['3.6.0', '3.6.1', '3.6.2'] # A set of versions known to produce the same output
 accepted_file_extensions = ('.h', '.cpp') # Files to format
-
-def check_clang_format_version(clang_format_exe):
-    try:
-        output = subprocess.check_output([clang_format_exe, '-version'])
-        for ver in tested_versions:
-            if ver in output:
-                print "Detected clang-format version " + ver
-                return
-        raise RuntimeError("Untested version: " + output)
-    except Exception as e:
-        print 'Could not verify version of ' + clang_format_exe + '.'
-        raise e
 
 def check_command_line_args(argv):
     required_args = ['{clang-format-exe}', '{files}']
@@ -46,7 +33,7 @@ def run_clang_format(clang_format_exe, files):
             for path, dirs, files in os.walk(target):
                 run_clang_format(clang_format_exe, (os.path.join(path, f) for f in files))
         elif target.endswith(accepted_file_extensions):
-            print "Format " + target
+            print "Formatting " + target
             subprocess.check_call([clang_format_exe, '-i', '-style=file', target], stdout=open(os.devnull, 'wb'), stderr=subprocess.STDOUT)
         else:
             print "Skip " + target
@@ -55,7 +42,6 @@ def main(argv):
     check_command_line_args(argv)
     clang_format_exe = argv[1]
     files = argv[2:]
-    check_clang_format_version(clang_format_exe)
     run_clang_format(clang_format_exe, files)
 
 if __name__ == "__main__":
