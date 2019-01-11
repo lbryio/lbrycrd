38810be1232e25563d25035acf3123fd63db8ee0
Brannon King <countprimes@gmail.com>
2019-01-11 12:19:57 +0100

    repaired ICU link order

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -3,6 +3,7 @@ $(package)_version=1_59_0
 $(package)_download_path=http://sourceforge.net/projects/boost/files/boost/1.59.0
 $(package)_file_name=$(package)_$($(package)_version).tar.bz2
 $(package)_sha256_hash=727a932322d94287b62abb1bd2d41723eec4356a7728909e38adb65ca25241ca
+$(package)_dependencies:icu
 
 define $(package)_set_vars
 $(package)_config_opts_release=variant=release
--- packaging/build_windows.sh
+++ packaging/build_windows.sh
@@ -10,6 +10,8 @@ sudo apt-get install -y --no-install-recommends \
 
 #################################################################
 # Build ICU for Linux first so that we can cross compile it below
+# It's a strange ICU thing in that it requries a working 
+# Linux build of itself to be used as part of the cross-compile
 #################################################################
 staging_dir=/tmp/icu_staging
 icu_linux_dir=$staging_dir/build_icu_linux
@@ -18,13 +20,15 @@ pushd $staging_dir
 wget -c http://download.icu-project.org/files/icu4c/57.1/icu4c-57_1-src.tgz
 tar -xvzf icu4c-57_1-src.tgz
 pushd icu/source
-./runConfigureICU Linux --prefix=$icu_linux_dir --enable-extras=no --enable-strict=no -enable-static --enable-shared=no --enable-tests=no --enable-samples=no --enable-dyload=no
+./runConfigureICU Linux --prefix=$icu_linux_dir --enable-extras=no --enable-strict=no --enable-static --enable-shared=no --enable-tests=no --enable-samples=no --enable-dyload=no
 make
 make install
 popd
 popd
 
 cd depends
+mkdir -p sources
+cp "$staging_dir/icu4c-57_1-src.tgz" sources/
 make HOST=i686-w64-mingw32 NO_QT=1
 cd ..
 patch -p1 < packaging/remove_consensus.patch
