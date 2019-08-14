32e6a99ee60c1997410c9ce863e463523372be6e
Anthony Fieroni <bvbfan@abv.bg>
2019-08-14 19:56:21 +0300

    Logic fixes, unit test
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/test/claimtriebranching_tests.cpp
+++ src/test/claimtriebranching_tests.cpp
@@ -4135,9 +4135,9 @@ BOOST_AUTO_TEST_CASE(update_on_support2_test)
     BOOST_CHECK_EQUAL(node.nHeightOfLastTakeover, height + 1);
 }
 
-void ValidatePairs(CClaimTrieCache& cache, const CClaimTrieProof& proof, uint256 claimHash)
+void ValidatePairs(CClaimTrieCache& cache, const std::vector<std::pair<bool, uint256>>& pairs, uint256 claimHash)
 {
-    for (auto& pair : proof.pairs)
+    for (auto& pair : pairs)
         if (pair.first) // we're on the right because we were an odd index number
             claimHash = Hash(pair.second.begin(), pair.second.end(), claimHash.begin(), claimHash.end());
         else
@@ -4182,7 +4182,7 @@ BOOST_AUTO_TEST_CASE(hash_includes_all_claims_single_test)
     BOOST_CHECK(proof.hasValue);
     BOOST_CHECK_EQUAL(proof.outPoint, outPoint);
     auto claimHash = getValueHash(outPoint, proof.nHeightOfLastTakeover);
-    ValidatePairs(fixture, proof, claimHash);
+    ValidatePairs(fixture, proof.pairs, claimHash);
 }
 
 BOOST_AUTO_TEST_CASE(hash_includes_all_claims_triple_test)
@@ -4212,7 +4212,7 @@ BOOST_AUTO_TEST_CASE(hash_includes_all_claims_triple_test)
             BOOST_CHECK(proof.hasValue);
             BOOST_CHECK_EQUAL(proof.outPoint, claim.outPoint);
             uint256 claimHash = getValueHash(claim.outPoint, proof.nHeightOfLastTakeover);
-            ValidatePairs(fixture, proof, claimHash);
+            ValidatePairs(fixture, proof.pairs, claimHash);
         }
     }
 }
@@ -4243,7 +4243,7 @@ BOOST_AUTO_TEST_CASE(hash_includes_all_claims_branched_test)
             BOOST_CHECK(proof.hasValue);
             BOOST_CHECK_EQUAL(proof.outPoint, claim.outPoint);
             uint256 claimHash = getValueHash(claim.outPoint, proof.nHeightOfLastTakeover);
-            ValidatePairs(fixture, proof, claimHash);
+            ValidatePairs(fixture, proof.pairs, claimHash);
         }
     }
 }
@@ -4278,9 +4278,186 @@ BOOST_AUTO_TEST_CASE(hash_claims_children_fuzzer_test)
             BOOST_CHECK(proof.hasValue);
             BOOST_CHECK_EQUAL(proof.outPoint, claim.outPoint);
             uint256 claimHash = getValueHash(claim.outPoint, proof.nHeightOfLastTakeover);
-            ValidatePairs(fixture, proof, claimHash);
+            ValidatePairs(fixture, proof.pairs, claimHash);
         }
     }
+}
+
+std::vector<std::pair<bool, uint256>> jsonToPairs(const UniValue& jsonPair)
+{
+    std::vector<std::pair<bool, uint256>> pairs;
+    for (std::size_t i = 0; i < jsonPair.size(); ++i) {
+        auto& jpair = jsonPair[i];
+        pairs.emplace_back(jpair[T_ODD].get_bool(), uint256S(jpair[T_HASH].get_str()));
+    }
+    return pairs;
+}
+
+BOOST_AUTO_TEST_CASE(hash_bid_seq_claim_changes_test)
+{
+    ClaimTrieChainFixture fixture;
+    fixture.setHashForkHeight(2);
+    fixture.IncrementBlocks(4);
+
+    std::string name = "toa";
+    std::string value1 = "one", value2 = "two", value3 = "tre", value4 = "for";
+
+    auto tx1 = fixture.MakeClaim(fixture.GetCoinbase(), name, value1, 1);
+    fixture.IncrementBlocks(1);
+
+    CMutableTransaction tx2 = BuildTransaction(fixture.GetCoinbase(), 0, 2);
+    tx2.vout[0].scriptPubKey = CScript() << OP_CLAIM_NAME
+                                         << std::vector<unsigned char>(name.begin(), name.end())
+                                         << std::vector<unsigned char>(value2.begin(), value2.end()) << OP_2DROP << OP_DROP << OP_TRUE;
+    tx2.vout[0].nValue = 3;
+    tx2.vout[1].scriptPubKey = CScript() << OP_CLAIM_NAME
+                                         << std::vector<unsigned char>(name.begin(), name.end())
+                                         << std::vector<unsigned char>(value3.begin(), value3.end()) << OP_2DROP << OP_DROP << OP_TRUE;
+    tx2.vout[1].nValue = 2;
+
+    fixture.CommitTx(tx2);
+    fixture.IncrementBlocks(1);
+
+    auto tx3 = fixture.MakeClaim(fixture.GetCoinbase(), name, value4, 4);
+    fixture.IncrementBlocks(1);
+
+    int height = chainActive.Height();
+
+    auto claimId1 = ClaimIdHash(tx1.GetHash(), 0);
+    auto claimId2 = ClaimIdHash(tx2.GetHash(), 0);
+    auto claimId3 = ClaimIdHash(tx2.GetHash(), 1);
+    auto claimId4 = ClaimIdHash(tx3.GetHash(), 0);
+
+    int claim1bid = 3, claim1seq = 0;
+    int claim2bid = 1, claim2seq = 1;
+    int claim3bid = 2, claim3seq = 2;
+    int claim4bid = 0, claim4seq = 3;
+
+    auto getclaimsforname = tableRPC["getclaimsforname"]->actor;
+
+    JSONRPCRequest req;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+
+    auto result = getclaimsforname(req);
+    auto claims = result[T_CLAIMS];
+    BOOST_CHECK_EQUAL(claims.size(), 4U);
+    BOOST_CHECK_EQUAL(result[T_LASTTAKEOVERHEIGHT].get_int(), height);
+    BOOST_CHECK_EQUAL(claims[0][T_EFFECTIVEAMOUNT].get_int(), 4);
+    BOOST_CHECK_EQUAL(claims[0][T_BID].get_int(), claim4bid);
+    BOOST_CHECK_EQUAL(claims[0][T_SEQUENCE].get_int(), claim4seq);
+    BOOST_CHECK_EQUAL(claims[0][T_CLAIMID].get_str(), claimId4.GetHex());
+    BOOST_CHECK_EQUAL(claims[1][T_EFFECTIVEAMOUNT].get_int(), 3);
+    BOOST_CHECK_EQUAL(claims[1][T_BID].get_int(), claim2bid);
+    BOOST_CHECK_EQUAL(claims[1][T_SEQUENCE].get_int(), claim2seq);
+    BOOST_CHECK_EQUAL(claims[1][T_CLAIMID].get_str(), claimId2.GetHex());
+    BOOST_CHECK_EQUAL(claims[2][T_EFFECTIVEAMOUNT].get_int(), 2);
+    BOOST_CHECK_EQUAL(claims[2][T_BID].get_int(), claim3bid);
+    BOOST_CHECK_EQUAL(claims[2][T_SEQUENCE].get_int(), claim3seq);
+    BOOST_CHECK_EQUAL(claims[2][T_CLAIMID].get_str(), claimId3.GetHex());
+    BOOST_CHECK_EQUAL(claims[3][T_EFFECTIVEAMOUNT].get_int(), 1);
+    BOOST_CHECK_EQUAL(claims[3][T_BID].get_int(), claim1bid);
+    BOOST_CHECK_EQUAL(claims[3][T_SEQUENCE].get_int(), claim1seq);
+    BOOST_CHECK_EQUAL(claims[3][T_CLAIMID].get_str(), claimId1.GetHex());
+
+    auto getclaimbybid = tableRPC["getclaimbybid"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(claim3bid));
+
+    result = getclaimbybid(req);
+    BOOST_CHECK_EQUAL(result[T_LASTTAKEOVERHEIGHT].get_int(), height);
+    BOOST_CHECK_EQUAL(result[T_EFFECTIVEAMOUNT].get_int(), 2);
+    BOOST_CHECK_EQUAL(result[T_BID].get_int(), claim3bid);
+    BOOST_CHECK_EQUAL(result[T_SEQUENCE].get_int(), claim3seq);
+    BOOST_CHECK_EQUAL(result[T_CLAIMID].get_str(), claimId3.GetHex());
+
+    auto getclaimbyseq = tableRPC["getclaimbyseq"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(claim2seq));
+
+    result = getclaimbyseq(req);
+    BOOST_CHECK_EQUAL(result[T_LASTTAKEOVERHEIGHT].get_int(), height);
+    BOOST_CHECK_EQUAL(result[T_EFFECTIVEAMOUNT].get_int(), 3);
+    BOOST_CHECK_EQUAL(result[T_BID].get_int(), claim2bid);
+    BOOST_CHECK_EQUAL(result[T_SEQUENCE].get_int(), claim2seq);
+    BOOST_CHECK_EQUAL(result[T_CLAIMID].get_str(), claimId2.GetHex());
+
+    auto getclaimbyid = tableRPC["getclaimbyid"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(claimId1.GetHex()));
+
+    result = getclaimbyid(req);
+    BOOST_CHECK_EQUAL(result[T_LASTTAKEOVERHEIGHT].get_int(), height);
+    BOOST_CHECK_EQUAL(result[T_EFFECTIVEAMOUNT].get_int(), 1);
+    BOOST_CHECK_EQUAL(result[T_BID].get_int(), claim1bid);
+    BOOST_CHECK_EQUAL(result[T_SEQUENCE].get_int(), claim1seq);
+    BOOST_CHECK_EQUAL(result[T_CLAIMID].get_str(), claimId1.GetHex());
+
+    // check by partial id (at least 3 chars)
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(claimId3.GetHex().substr(0, 3)));
+
+    result = getclaimbyid(req);
+    BOOST_CHECK_EQUAL(result[T_LASTTAKEOVERHEIGHT].get_int(), height);
+    BOOST_CHECK_EQUAL(result[T_EFFECTIVEAMOUNT].get_int(), 2);
+    BOOST_CHECK_EQUAL(result[T_BID].get_int(), claim3bid);
+    BOOST_CHECK_EQUAL(result[T_SEQUENCE].get_int(), claim3seq);
+    BOOST_CHECK_EQUAL(result[T_CLAIMID].get_str(), claimId3.GetHex());
+
+    auto blockhash = chainActive.Tip()->GetBlockHash();
+
+    auto getnameproof = tableRPC["getnameproof"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(blockhash.GetHex()));
+    req.params.push_back(UniValue(claimId3.GetHex()));
+
+    result = getnameproof(req);
+    auto claimHash = getValueHash(COutPoint(tx2.GetHash(), 1), result[T_LASTTAKEOVERHEIGHT].get_int());
+    ValidatePairs(fixture, jsonToPairs(result[T_PAIRS]), claimHash);
+
+    // check by partial id (can be even 1 char)
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(blockhash.GetHex()));
+    req.params.push_back(UniValue(claimId2.GetHex().substr(0, 2)));
+
+    result = getnameproof(req);
+    claimHash = getValueHash(COutPoint(tx2.GetHash(), 0), result[T_LASTTAKEOVERHEIGHT].get_int());
+    ValidatePairs(fixture, jsonToPairs(result[T_PAIRS]), claimHash);
+
+    auto getclaimproofbybid = tableRPC["getclaimproofbybid"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(claim1bid));
+
+    result = getclaimproofbybid(req);
+    claimHash = getValueHash(COutPoint(tx1.GetHash(), 0), result[T_LASTTAKEOVERHEIGHT].get_int());
+    ValidatePairs(fixture, jsonToPairs(result[T_PAIRS]), claimHash);
+
+    auto getclaimproofbyseq = tableRPC["getclaimproofbyseq"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(name));
+    req.params.push_back(UniValue(claim4seq));
+
+    result = getclaimproofbyseq(req);
+    claimHash = getValueHash(COutPoint(tx3.GetHash(), 0), result[T_LASTTAKEOVERHEIGHT].get_int());
+    ValidatePairs(fixture, jsonToPairs(result[T_PAIRS]), claimHash);
+
+    auto getchangesinblock = tableRPC["getchangesinblock"]->actor;
+    req.params = UniValue(UniValue::VARR);
+    req.params.push_back(UniValue(blockhash.GetHex()));
+
+    result = getchangesinblock(req);
+    BOOST_CHECK_EQUAL(result[T_CLAIMSADDEDORUPDATED].size(), 3);
+    BOOST_CHECK_EQUAL(result[T_CLAIMSADDEDORUPDATED][0].get_str(), claimId2.GetHex());
+    BOOST_CHECK_EQUAL(result[T_CLAIMSADDEDORUPDATED][1].get_str(), claimId3.GetHex());
+    BOOST_CHECK_EQUAL(result[T_CLAIMSADDEDORUPDATED][2].get_str(), claimId4.GetHex());
+    BOOST_CHECK_EQUAL(result[T_CLAIMSREMOVED].size(), 0);
+    BOOST_CHECK_EQUAL(result[T_SUPPORTSADDEDORUPDATED].size(), 0);
+    BOOST_CHECK_EQUAL(result[T_SUPPORTSREMOVED].size(), 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
