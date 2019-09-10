da86c108e7a064fbac0c10821704b4ccf2819bcf
Anthony Fieroni <bvbfan@abv.bg>
2019-09-10 14:46:55 +0300

    Show correct script ops in asm
    Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

--- src/script/script.cpp
+++ src/script/script.cpp
@@ -132,9 +132,9 @@ const char* GetOpName(opcodetype opcode)
     case OP_CHECKSEQUENCEVERIFY    : return "OP_CHECKSEQUENCEVERIFY";
     case OP_NOP4                   : return "OP_NOP4";
     case OP_NOP5                   : return "OP_NOP5";
-    case OP_NOP6                   : return "OP_NOP6";
-    case OP_NOP7                   : return "OP_NOP7";
-    case OP_NOP8                   : return "OP_NOP8";
+    case OP_NOP6                   : return "OP_CLAIM_NAME";
+    case OP_NOP7                   : return "OP_SUPPORT_CLAIM";
+    case OP_NOP8                   : return "OP_UPDATE_CLAIM";
     case OP_NOP9                   : return "OP_NOP9";
     case OP_NOP10                  : return "OP_NOP10";
 
