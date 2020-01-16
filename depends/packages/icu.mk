f3170e1c85e2b9ddea613794b8ab6c5d92d094cd
Brannon King <countprimes@gmail.com>
2020-01-15 21:37:12 -0700

    increase version, fix windows build

--- depends/packages/icu.mk
+++ depends/packages/icu.mk
@@ -21,7 +21,6 @@ define $(package)_preprocess_cmds
   PKG_CONFIG_SYSROOT_DIR=/ \
   PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
   PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
-  sed -i.old 's/^GEN_DEPS.cc.*/& $(CXXFLAGS)/' source/config/mh-mingw* && \
   mkdir -p build && cd build && \
   ../source/runConfigureICU Linux $($(package)_standard_opts) CXXFLAGS=-std=c++11 && \
   $(MAKE) && cd ..
@@ -31,6 +30,8 @@ define $(package)_config_cmds
   PKG_CONFIG_SYSROOT_DIR=/ \
   PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig \
   PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
+  sed -i.old 's/^GEN_DEPS.c=.*/& $($(package)_cflags)/' config/mh-mingw* && \
+  sed -i.old 's/^GEN_DEPS.cc=.*/& $($(package)_cxxflags)/' config/mh-mingw* && \
   $($(package)_autoconf)
 endef
 
